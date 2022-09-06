a6592b053ecc78ff97d0faac887a0f558b9cdfbe
Albert Khazipov <aaooo00aa0@gmail.com>
2022-09-06 12:00:18 +0000

    Week 1 solutions

---
+++ week01/ex1.sh
@@ -0,0 +1,1 @@
+ls /usr/bin | sort -r | grep "gcc" | tail -n 5 > ex1.txt
---
+++ week01/ex3.sh
@@ -0,0 +1,15 @@
+date
+sleep 3
+mkdir folderRoot
+date
+sleep 3
+mkdir folderHome
+cd folderRoot
+ls -t -r /
+date
+sleep 3
+ls -t -r / > root.txt
+cd ~/week01/folderHome
+date
+sleep 3
+ls -t -r ~ > home.txt
